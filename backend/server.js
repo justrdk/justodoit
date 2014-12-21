a89789a74367ebbf426f17c1f53315a2241dfbd3
Mario Maradiaga <levrak91@gmail.com>
2014-12-20 23:00:06 -0600

    Added simple backend examples

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,19 @@ var path = require('path');
 var router = express.Router();
 
 
-router.use(function(req, res, next) {
-	console.log(req.method, req.url);
-	next();
-});
 
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-var resolvedPath = path.resolve('/../public/index.html');
+
+router.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
 
 app.use('/libuniversal', router);
 app.get('*', function(req, res) {
@@ -26,29 +26,45 @@ app.get('*', function(req, res) {
 });
 
 app.listen(port);
+
 console.log("App listening on port " + port);
 
 
+var resolvedPath = path.resolve('/../public/index.html');
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/justdoit';
 MongoClient.connect(url, function(err, db) {
-	var collection = db.collection('producto');
-	collection.find({}).toArray(function(err, docs){
-		console.log("Existen " + docs.length + "registros:")
-	  	if(docs.length < 5){
-	  		console.log("Existen menos de 5 registros, agreguemos 2")
-	  		collection.insert([{nombre: "Mario", valor: 10}, {nombre: "Osmin", valor: 20}], function(err, result){
-			  	console.log("Registros agregados: ")
-			  	console.log(result)
-			  	db.close()
-			})
-	  	}else if(docs.length > 5){
-	  		console.log("Existen mas de 5 registros, borremos todo")
-			collection.remove({}, function(err, result){
-				console.log( "Registros borrados: ")
-				console.log(result)
-			  	db.close()
-			})
-	  	}
-	 })
+	router.get('/producto/list', function(req, res){
+		db.collection('producto').find({}).toArray(function(err, docs){
+			res.json(docs);
+		})
+	})
+
+	router.post('/producto/create', function(req, res){
+		db.collection('producto').insert([{nombre: req.body.nombre, valor: req.body.valor}], function(err, result){
+		  	console.log("Registros creados: ")
+		  	res.json(result)
+		  	db.close()
+		})
+	})
+
+	// var collection = db.collection('producto');
+	// collection.find({}).toArray(function(err, docs){
+	// 	console.log("Existen " + docs.length + "registros:")
+	//   	if(docs.length < 5){
+	//   		console.log("Existen menos de 5 registros, agreguemos 2")
+	//   		collection.insert([{nombre: "Mario", valor: 10}, {nombre: "Osmin", valor: 20}], function(err, result){
+	// 		  	console.log("Registros agregados: ")
+	// 		  	console.log(result)
+	// 		  	db.close()
+	// 		})
+	//   	}else if(docs.length > 5){
+	//   		console.log("Existen mas de 5 registros, borremos todo")
+	// 		collection.remove({}, function(err, result){
+	// 			console.log( "Registros borrados: ")
+	// 			console.log(result)
+	// 		  	db.close()
+	// 		})
+	//   	}
+	//  })
 });
