c5599125d08029c903072a59b1e997049b38449f
evilryu <osman.sist@gmail.com>
2015-04-29 22:51:13 -0600

    added provider crud operations

---
+++ backend/controllers/provider.js
@@ -0,0 +1,37 @@
+module.exports = {
+	getAllProviders : function(request, reply){
+		var providerService = require('../services/providerService');
+
+		providerService.getAllProviders(request, function(data){
+			reply(data);
+		});
+	},
+	getProvider : function(request, reply){
+		var providerService = require('../services/providerService');
+
+		providerService.getProvider(request, function(data){
+			reply(data);
+		});
+	},
+	createProvider : function(request, reply){
+		var providerService = require('../services/providerService');
+
+		providerService.createProvider(request, function(data){
+			reply(data);
+		});
+	},
+	updateProvider : function(request, reply){
+		var providerService = require('../services/providerService');
+
+		providerService.updateProvider(request, function(data){
+			reply(data);
+		});
+	},
+	deleteProvider : function(request, reply){
+		var providerService = require('../services/providerService');
+
+		providerService.deleteProvider(request, function(data){
+			reply(data);
+		});
+	}
+};
